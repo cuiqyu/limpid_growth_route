e6d11599ff76b2e78ecbf07b59801633316526e1
cuiqiongyu <cuiqiongyu@unilifemedia.cn>
2020-01-14 11:19:54 +0800

    hystrix实现单台服务器故障避免服务器连锁反应，实现断路由操作

--- springcloud/service_ribbon/src/main/java/com/limpid/service_ribbon/service/impl/HelloServiceImpl.java
+++ springcloud/service_ribbon/src/main/java/com/limpid/service_ribbon/service/impl/HelloServiceImpl.java
@@ -1,6 +1,7 @@
 package com.limpid.service_ribbon.service.impl;
 
 import com.limpid.service_ribbon.service.HelloService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,7 +17,13 @@ public class HelloServiceImpl implements HelloService {
     private RestTemplate restTemplate;
 
     @Override
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService() {
         return restTemplate.getForObject("http://service-li/hello/world", String.class);
     }
+
+    public String hiError() {
+        return "hi, sorry error";
+    }
+
 }
