dec8127d8304d4f52876302306ebcdc06781040b
cuiqiongyu <cuiqiongyu@unilifemedia.cn>
2020-01-15 13:50:37 +0800

    spring cloud config 使用分布式配置中心实现

--- springcloud/config_server/src/main/java/com/limpid/config_server/Application.java
+++ springcloud/config_server/src/main/java/com/limpid/config_server/Application.java
@@ -3,9 +3,11 @@ package com.limpid.config_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class Application {
 
     public static void main(String[] args) {
